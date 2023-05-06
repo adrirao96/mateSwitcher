2321194613c72501da4d0c32e1ad1c4ce46d873e
Adrian Rao <adrirao.96@gmail.com>
2023-05-06 09:21:46 -0300

    se descarga la libreria de iconos, y se agrega el componente Switcher

--- app/src/main/java/dev/ar/mateswitcher/MainActivity.kt
+++ app/src/main/java/dev/ar/mateswitcher/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.ar.mateswitcher.ui.theme.MateSwitcherTheme
@@ -16,14 +20,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MateSwitcherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            var darkTheme by remember {
+                mutableStateOf(false)
+            }
+            MateSwitcherTheme(darkTheme = darkTheme) {
+
             }
         }
     }
